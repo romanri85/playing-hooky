54dd481bec1081518b27a37ca1c5441ed8423fd5
romanri85 <63948790+romanri85@users.noreply.github.com>
2023-05-31 22:50:31 +0300

    add var a

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let a = 10
 root.render(
   <React.StrictMode>
     <App />
